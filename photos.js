99fe421ae834a3801af272a791b1583f80e04ca6
Juan Diego Jimenez <jdiejim@gmail.com>
2017-04-01 17:51:17 -0600

    add pics; slideshow fn

---
+++ photos.js
@@ -0,0 +1,38 @@
+var photoIndex = 1;
+$('.arrows-container').on('click', function(e) {
+  var targetId = e.target.id;
+  switch (targetId) {
+    case 'next':
+      photoIndex++;
+      break;
+    case 'back':
+      photoIndex--;
+      break;
+  }
+  if(photoIndex > 3) {
+    photoIndex = 1;
+  }
+  if(photoIndex < 1) {
+    photoIndex = 3;
+  }
+  updateGraphics(photoIndex);
+});
+
+function updateGraphics(photoIndex) {
+  $('.graphics').css('background-image', 'url(Images/profile-pic-' + photoIndex + '.jpg)');
+  $('#counter' + photoIndex).css('background-color', 'white');
+  switch (photoIndex) {
+    case 1:
+      $('#counter2').css('background-color', 'black');
+      $('#counter3').css('background-color', 'black');
+      break;
+    case 2:
+      $('#counter1').css('background-color', 'black');
+      $('#counter3').css('background-color', 'black');
+      break;
+    case 3:
+      $('#counter1').css('background-color', 'black');
+      $('#counter2').css('background-color', 'black');
+      break;
+  }
+}
